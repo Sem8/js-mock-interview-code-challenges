662c14ce63df835f4b658c65e80a77c7f14037df
Sem8 <mssemmi8@gmail.com>
2019-09-05 18:43:01 -0400

    wrote the memoizer function for getting the nth fibonacci number function to speed up recursive functions

--- nthFibonacciNumber.js
+++ nthFibonacciNumber.js
@@ -1,21 +1,37 @@
 // Starting 1st number with index of 1
-// const fib = (n) => {
-//     return n == 1 ? 0 :
-//            n == 2 ? 1 :
-//            fib(n-1) + fib(n-2);
-// } 
+const fib = (n) => {
+    return n == 1 ? 0 :
+           n == 2 ? 1 :
+           fib(n-1) + fib(n-2);
+} 
 
 // Labeling 1st number as 0 index
-const fib = (n) => {
-    let fibo = [0, 1];
+// const fib = (n) => {
+//     let fibo = [0, 1];
 
-    if (n <= 2) return 1;
+//     if (n <= 2) return 1;
 
-    for (let i = 2; i <= n; i++) {
-        fibo[i] = fibo[i - 1] + fibo[i - 2];
+//     for (let i = 2; i <= n; i++) {
+//         fibo[i] = fibo[i - 1] + fibo[i - 2];
+//     }
+
+//     return fibo[n]
+// }
+
+const memoizer = (func) => {
+    let cache = [];
+
+    return function(n) {
+        let idx = n.toString();
+        if(cache[idx] == undefined) {
+            cache[idx] = func(n);
+        }
+        return cache[idx];
     }
-
-    return fibo[n]
 }
 
-console.log(fib(12));
+// console.log(fib(23));
+
+const mem_fib = memoizer(fib);
+console.log(mem_fib(23));
+
