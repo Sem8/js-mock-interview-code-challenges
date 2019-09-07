d69d7927f3fca57156403542ef3b9bf79e707da1
Sem8 <mssemmi8@gmail.com>
2019-09-07 16:34:17 -0400

    using one liner short concise solution to remove duplicates from an array using Sets

--- removeDuplicatesFromArray.js
+++ removeDuplicatesFromArray.js
@@ -13,17 +13,26 @@
 // }
 
 // For maximal performance:
-const removeDuplicate = (arr) => {
-  let hashTable = {};
-  let arrChecker = [];
-  let j = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if(hashTable[arr[i]] !== -1) {
-      hashTable[arr[i]] = 1;
-      arrChecker[j++] = arr[i]
-    }
-  }
-  return arrChecker;
-}
+// const removeDuplicate = (arr) => {
+//   let hashTable = {};
+//   let arrChecker = [];
+//   let j = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if(hashTable[arr[i]] !== 1) {
+//       hashTable[arr[i]] = 1;
+//       arrChecker[j++] = arr[i]
+//     }
+//   }
+//   return arrChecker;
+// }
+
+// Short and concise one liner solution using set
+// const removeDuplicate = (arr) => {
+//   return [...new Set(arr)];
+// }
+
+// Using filter
+const removeDuplicate = (arr) => {}
+
 
   console.log(removeDuplicate([1,3,3,3,1,5,6,7,8,1])); // [1, 3, 5, 6, 7, 8]
