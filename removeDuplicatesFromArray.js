67837d3b5f84a73c6caa1bc41ade689e1f6cd99f
Sem8 <mssemmi8@gmail.com>
2019-09-07 16:29:30 -0400

    wrote a more performant algorithm to remove duplicates from an array without using array push

--- removeDuplicatesFromArray.js
+++ removeDuplicatesFromArray.js
@@ -1,13 +1,29 @@
+// Good enough solution
+// const removeDuplicate = (arr) => {
+//   let hashTable = {};
+//   let arrChecker = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if(!hashTable[arr[i]]) {
+//       arrChecker.push(arr[i]);
+//       hashTable[arr[i]] = true;
+//     }
+//   }
+//   return arrChecker;
+// }
+
+// For maximal performance:
 const removeDuplicate = (arr) => {
   let hashTable = {};
   let arrChecker = [];
-
+  let j = 0;
   for (let i = 0; i < arr.length; i++) {
-    if(!hashTable[arr[i]]) {
-      arrChecker.push(arr[i]);
-      hashTable[arr[i]] = true;
+    if(hashTable[arr[i]] !== -1) {
+      hashTable[arr[i]] = 1;
+      arrChecker[j++] = arr[i]
     }
   }
   return arrChecker;
 }
+
   console.log(removeDuplicate([1,3,3,3,1,5,6,7,8,1])); // [1, 3, 5, 6, 7, 8]
