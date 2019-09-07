f5c30c9d124a6cd766beaf8760af2fa22b9a6cd7
Sem8 <mssemmi8@gmail.com>
2019-09-07 16:53:54 -0400

    wrote code for solution to removing duplicates from an array using array filter method

--- removeDuplicatesFromArray.js
+++ removeDuplicatesFromArray.js
@@ -32,7 +32,14 @@
 // }
 
 // Using filter
-const removeDuplicate = (arr) => {}
+// const removeDuplicate = (arr) => {
+//   return arr.filter((item, index) => arr.indexOf(item) === index);
+// }
+
+// Using reduce method
+const removeDuplicate = (arr) => {
+  
+}
 
 
   console.log(removeDuplicate([1,3,3,3,1,5,6,7,8,1])); // [1, 3, 5, 6, 7, 8]
