b65541bd131566fdd4f19b73854467bc694a4da3
Sem8 <mssemmi8@gmail.com>
2019-09-07 15:37:20 -0400

    included 2 different solutions to code challenge checking whether all the elements from the first array are included in a second array

---
+++ arrayContainsAllElements.js
@@ -0,0 +1,20 @@
+// check to see if all items from the first array are included in the second array
+
+// Solution using every() and includes() method
+// const ifFirstArrayIsIncluded = (arr1, arr2) => {
+//     return arr1.every(eachElem => arr2.includes(eachElem));
+// }
+
+// Solution using every() and indexOf() method
+// const ifFirstArrayIsIncluded = (arr1, arr2) => {
+//     return arr1.every(eachElem => arr2.indexOf(eachElem) !== -1);
+// }
+
+// using pure Javascript
+arrOne = [1, 2, 3, 4, 5]; // target array
+arrTwo = [4, 5, 1, 2, 3, 9, 10]; // true
+arrThree = [4, 5, 8, 9, 10]; // false
+arrFour = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14]; // false
+arrFive = [10, 20, 30, 40, 50, 60, 70]; // false
+
+console.log(ifFirstArrayIsIncluded(arrOne, arrTwo));
---
+++ removeDuplicatesFromArray.js
@@ -0,0 +1,2 @@
+
+  console.log(removeDuplicate([1,3,3,3,1,5,6,7,8,1]));
