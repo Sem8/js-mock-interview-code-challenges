6c7c2112052c97c524466e32bb9189bb2a24f117
Sem8 <mssemmi8@gmail.com>
2019-09-07 16:04:05 -0400

    included solution using pure javascript for function for checking whether all elements from one array are included in the second array

--- arrayContainsAllElements.js
+++ arrayContainsAllElements.js
@@ -11,10 +11,26 @@
 // }
 
 // using pure Javascript
+const ifFirstArrayIsIncluded = (arr1, arr2) => {
+    if (arr1.length <= arr2.length) {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr2.indexOf(arr1[i]) == -1) {
+                return false;
+            }
+        }
+    } else {
+        return false
+    }
+    return true;
+}
+
+
+
+
 arrOne = [1, 2, 3, 4, 5]; // target array
 arrTwo = [4, 5, 1, 2, 3, 9, 10]; // true
 arrThree = [4, 5, 8, 9, 10]; // false
 arrFour = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14]; // false
 arrFive = [10, 20, 30, 40, 50, 60, 70]; // false
 
-console.log(ifFirstArrayIsIncluded(arrOne, arrTwo));
+console.log(ifFirstArrayIsIncluded(arrOne, arrThree));
