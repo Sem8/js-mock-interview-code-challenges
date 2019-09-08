feb56bdee18ab24b491136b70f913a6831df8a8f
Sem8 <mssemmi8@gmail.com>
2019-09-08 18:38:21 -0400

    wrote solution for reversing a string iteratively when string concatenation is not supported

--- reverseString.js
+++ reverseString.js
@@ -1,22 +1,31 @@
 // Reverse string:
-const reverseStr = str => {
-    let reversedString = '';
+// const reverseStr = str => {
+//     let reversedString = '';
 
-    for (let i = str.length -1; i >= 0; i--) {
-        reversedString += str[i];
+//     for (let i = str.length -1; i >= 0; i--) {
+//         reversedString += str[i];
+//     }
+//     return reversedString;
+// }
+
+// Reverse string when string concatenation is not supported by a browser:
+const reverseStr = str => {
+    let revStrArr = [];
+    for (let i = str.length -1; i >=0; i--) {
+        revStrArr.push(str[i]);
     }
-    return reversedString;
+    return revStrArr.join('');
 }
 
 
 
 // Reverse string recursively
-const reverseStr = (str) => {
-    if (str === '') {
-        return '';
-    }
-    return reverseStr(str.substr(1)) + str.charAt(0);    
-}
+// const reverseStr = (str) => {
+//     if (str === '') {
+//         return '';
+//     }
+//     return reverseStr(str.substr(1)) + str.charAt(0);    
+// }
 
 
 console.log(reverseStr('string'));
