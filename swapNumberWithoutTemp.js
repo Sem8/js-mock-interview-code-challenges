69bc7ff353196a68e17694fbee38793e1ccbc747
Sem8 <mssemmi8@gmail.com>
2019-09-08 17:06:48 -0400

    wrote code for swapping 2 numbers without using a temporary variable, this time with bit manipulation

--- swapNumberWithoutTemp.js
+++ swapNumberWithoutTemp.js
@@ -1,8 +1,15 @@
-const swapNumsWoutTemp = (num1, num2) => {
-    num2 = num2 - num1;
-    num1 = num1 + num2;
-    num2 = num1 - num2;
-    return [num1, num2];    
-}
+// const swapNumsWoutTemp = (num1, num2) => {
+//     num2 = num2 - num1;
+//     num1 = num1 + num2;
+//     num2 = num1 - num2;
+//     return [num1, num2];    
+// }
 
+// With bit manipulation:
+const swapNumsWoutTemp = (num1, num2) => {
+    num1 = num1 ^ num2;
+    num2 = num1 ^ num2;
+    num1 = num1 ^ num2;
+    return [num1, num2];
+}
 console.log(swapNumsWoutTemp(2, 3));
