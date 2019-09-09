2f0780220c5ec700d1d0a03bee0df0da5b8bfecd
Sem8 <mssemmi8@gmail.com>
2019-09-09 19:32:24 -0400

    3 solutions for finding the first non-repeating characters in a sentence complete

--- firstNonRepeatChar.js
+++ firstNonRepeatChar.js
@@ -1,24 +1,24 @@
 // Solution using a hashtable approach:
-// const findNonRepeatedCharacter = string => {
-//   let charObj = {};
-//   let len = string.length;
+const findNonRepeatedCharacter = string => {
+  let charObj = {};
+  let len = string.length;
 
-//   for (let i = 0; i < len; i++) {
-//     let char = string[i];
-//     if (charObj[char]) {
-//       charObj[char]++;
-//     } else {
-//       charObj[char] = 1;
-//     }
-//   }
+  for (let i = 0; i < len; i++) {
+    let char = string[i];
+    if (charObj[char]) {
+      charObj[char]++;
+    } else {
+      charObj[char] = 1;
+    }
+  }
 
-//   for (aLetter in charObj) {
-//     if (charObj[aLetter] == 1) {
-//       return aLetter;
-//     }
-//   }
-//   return null;
-// }
+  for (aLetter in charObj) {
+    if (charObj[aLetter] == 1) {
+      return aLetter;
+    }
+  }
+  return null;
+}
 
 // Solution using indexOf to find a letter
 // 1) Loop through string and set variable char to each letter in string
@@ -39,13 +39,13 @@
 // 2) If the firs index of a character with indexOf method equals the last index of a character with lastIndexOf method then it is a non-repeating 
 // letter so return that letter
 
-const findNonRepeatedCharacter = string => {
-  for (let i = 0; i < string.length; i++) {
-    if (string.indexOf(string[i]) === string.lastIndexOf(string[i])) {
-      return string[i];
-    }
-  }
-  return null
-}
+// const findNonRepeatedCharacter = string => {
+//   for (let i = 0; i < string.length; i++) {
+//     if (string.indexOf(string[i]) === string.lastIndexOf(string[i])) {
+//       return string[i];
+//     }
+//   }
+//   return null
+// }
 
 console.log(findNonRepeatedCharacter('the quick brown fox jumps then quickly blow air'))
