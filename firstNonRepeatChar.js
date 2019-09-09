6201f7b08646cca59c226d92684fa2935f80c5ef
Sem8 <mssemmi8@gmail.com>
2019-09-09 19:18:03 -0400

    wrote second solution code for finding first non-repeating character in a string using indexOf method and wrote out the pseduocode as well

--- firstNonRepeatChar.js
+++ firstNonRepeatChar.js
@@ -1,32 +1,37 @@
+// Solution using a hashtable approach:
+// const findNonRepeatedCharacter = string => {
+//   let charObj = {};
+//   let len = string.length;
+
+//   for (let i = 0; i < len; i++) {
+//     let char = string[i];
+//     if (charObj[char]) {
+//       charObj[char]++;
+//     } else {
+//       charObj[char] = 1;
+//     }
+//   }
+
+//   for (aLetter in charObj) {
+//     if (charObj[aLetter] == 1) {
+//       return aLetter;
+//     }
+//   }
+//   return null;
+// }
+
+// Solution using indexOf to find a letter
+// 1) Loop through string and set variable char to each letter in string
+// 2) if statement checks if a letter in the string is equal to the index and if the letter in the string at the next index does not exist
+// then return that letter
+
 const findNonRepeatedCharacter = string => {
-  let charObj = {};
-  let len = string.length;
-
-  for (let i = 0; i < len; i++) {
-    let char = string[i];
-    if (charObj[char]) {
-      charObj[char]++;
-    } else {
-      charObj[char] = 1;
-    }
-  }
-
-  for (aLetter in charObj) {
-    if (charObj[aLetter] == 1) {
-      return aLetter;
+  for (let i = 0; i < string.length; i++) {
+    if (string.indexOf(string[i]) == i && string.indexOf(string[i], i + 1) == -1) {
+      return string[i];
     }
   }
   return null;
 }
-
-// function firstNonRepeatedCharacter(string) {
-//     for (var i = 0; i < string.length; i++) {
-//       var c = string.charAt(i);
-//       if (string.indexOf(c) == i && string.indexOf(c, i + 1) == -1) {
-//         return c;
-//       }
-//     }
-//     return null;
-//   }
 
 console.log(findNonRepeatedCharacter('the quick brown fox jumps then quickly blow air'))
