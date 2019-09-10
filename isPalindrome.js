c92e5b60356ffcd6ef869539bc8a18619138c8d4
Sem8 <mssemmi8@gmail.com>
2019-09-10 14:57:01 -0400

    coded the manual solution to check if a string is palindrome or not

---
+++ isPalindrome.js
@@ -0,0 +1,11 @@
+const palindromeChecker = str => {
+    let len = str.length;
+    for (let i = 0; i < len/2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(palindromeChecker('madam'));
+console.log(palindromeChecker('toyota'));
