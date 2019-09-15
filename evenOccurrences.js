3457dc61e30177fa22375323b40cd3043b148edc
Sem8 <mssemmi8@gmail.com>
2019-09-15 16:33:38 -0400

    wrote solution code for finding first element with even number of occurrences in an array using someone else's slightly more optimized solution

--- evenOccurrences.js
+++ evenOccurrences.js
@@ -9,25 +9,45 @@
 // 4) Loop through hashTable with for/in loop and if value of a key value pair is divisible by 2 (hashTable[item] % 2 == 0) return 
 // that key item
 
+// My first pass approach solution
+// const evenOccurrence = arr => {
+//     let hashTable = {};
+//     for (let i = 0; i < arr.length; i++) {
+//         if (hashTable[arr[i]]) {
+//             hashTable[arr[i]]++;
+//         } else {
+//             hashTable[arr[i]] = 1;
+//         }
+//     }
+//     // console.log(hashTable);
+//     for (eachItem in hashTable) {
+//         if (hashTable[eachItem] % 2 === 0) {
+//             return eachItem;
+//         }
+//     }
+//     return null;
+// }
+
+// Someone else's solution using forEach to loop through array.
 const evenOccurrence = arr => {
-    let hashTable = {};
+    let storage = {};
+
+    arr.forEach((value, index) => {
+        storage[value] = storage[value] + 1 || 1;
+    });
+
     for (let i = 0; i < arr.length; i++) {
-        if (hashTable[arr[i]]) {
-            hashTable[arr[i]]++;
-        } else {
-            hashTable[arr[i]] = 1;
-        }
-    }
-    // console.log(hashTable);
-    for (eachItem in hashTable) {
-        if (hashTable[eachItem] % 2 === 0) {
-            return eachItem;
+        let current = arr[i];
+        if (storage[current] % 2 === 0) {
+            return current;
         }
     }
     return null;
-}
+};
 
-console.log(evenOccurrence([1, 2, 2, 3, 4, 4, 4, 4]))
+
+// console.log(evenOccurrence([1, 2, 2, 3, 4, 4, 4, 4]));
+console.log(evenOccurrence([1, 2, 2, 2, 2, 3, 4, 4, 4, 4, 4, 4]))
 
 
 
