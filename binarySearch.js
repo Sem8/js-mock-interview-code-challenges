44b0e30cf87c78464c4d30de247dbd647b4c1412
Sem8 <mssemmi8@gmail.com>
2019-09-26 22:24:48 -0400

    wrote the recursive solution for binary search algorithm to search and find an element within a sorted array with O(log n) time complexity and also wrote the pseudocode for the solution

--- binarySearch.js
+++ binarySearch.js
@@ -1,4 +1,4 @@
-// Iterative approach:
+// Iterative approach: O(log n) time complexity
 /*Pseudocode:
 1. Set a variable called startIndex and set it to 0 which will be the start index of the array at the beginning
 2. Intialize a variable called endIndex and set it to the last index of the array which will currently at the very beginning, be the index
@@ -18,26 +18,59 @@ is returned.
 8. Outside while loop, if element is not in array return 'element is not in array'
  */
 
-const binarySearch = (arr, target) => {
-    let startIndex = 0;
-    let endIndex = arr.length - 1;
+// const binarySearch = (arr, target) => {
+//     let startIndex = 0;
+//     let endIndex = arr.length - 1;
 
-    while (startIndex <= endIndex) {
-        let midIndex = Math.floor((startIndex + endIndex) / 2);
+//     while (startIndex <= endIndex) {
+//         let midIndex = Math.floor((startIndex + endIndex) / 2);
 
-        if (arr[midIndex] === target) {
-            return `Number is at index ${midIndex}`
-        }
-        else if (arr[midIndex] < target) {
-            startIndex = midIndex + 1;
-        } else {
-            endIndex = midIndex - 1;
-        }
-    }
-    return `Target is not found`;
+//         if (arr[midIndex] === target) {
+//             return `Number is at index ${midIndex}`
+//         }
+//         else if (arr[midIndex] < target) {
+//             startIndex = midIndex + 1;
+//         } else {
+//             endIndex = midIndex - 1;
+//         }
+//     }
+//     return `Target is not found`;
+// };
+
+// console.log(binarySearch([1, 3, 5, 7, 8, 9], 5)); // 2
+// console.log(binarySearch([1, 3, 5, 7, 8, 9], 8)); // 4
+// console.log(binarySearch([1, 3, 5, 7, 8, 9], 3)); // 1
+// console.log(binarySearch([1, 3, 5, 7, 8, 9], 6)); // Target is not found
+
+// Recursive solution:
+/*Pseudocode:
+1. Set a base case with if statement of if start index is more than end index return 'Target not found'
+2. Intialize a variable called midIndex and set it equal to the halfway point index number in the array using Math.floor method, where the
+start index and end index are summed and then divided by 2.
+3. Make if statement, check if element at middle index (midIndex) of array is greater than target input element, if so, return the recursive
+function where end input index will be the current middle index (midIndex) minus 1.
+4. Else (if array element at middle index or midIndex is less than the target input element), return the recursive function calling itself
+where the start input in the function will be set to the middle index (midIndex) plus 1. 
+ */
+
+const recursiveBinarySearch = (arr, target, start, end) => {
+  // base case
+  if (start > end) {
+    return `Target not found`;
+  }
+
+  let midIndex = Math.floor((start + end) / 2);
+
+  if (arr[midIndex] === target) {
+    return `Target element is at index ${midIndex}`;
+  } else if (arr[midIndex] > target) {
+    return recursiveBinarySearch(arr, target, start, midIndex - 1);
+  } else {
+    return recursiveBinarySearch(arr, target, midIndex + 1, end);
+  }
 };
 
-console.log(binarySearch([1, 3, 5, 7, 8, 9], 5)); // 2
-console.log(binarySearch([1, 3, 5, 7, 8, 9], 8)); // 4
-console.log(binarySearch([1, 3, 5, 7, 8, 9], 3)); // 1
-console.log(binarySearch([1, 3, 5, 7, 8, 9], 6)); // Target is not found
+console.log(recursiveBinarySearch([1, 3, 5, 7, 8, 9], 5, 0, 5)); // 2
+console.log(recursiveBinarySearch([1, 3, 5, 7, 8, 9], 8, 0, 5)); // 4
+console.log(recursiveBinarySearch([1, 3, 5, 7, 8, 9], 3, 0, 5)); // 1
+console.log(recursiveBinarySearch([1, 3, 5, 7, 8, 9], 6, 0, 5)); // Target is not found
