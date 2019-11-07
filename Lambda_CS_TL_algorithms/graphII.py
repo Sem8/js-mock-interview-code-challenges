ffb127e90784dd77ddf4578570c7abd7ac483fd5
Sem8 <mssemmi8@gmail.com>
2019-11-07 02:02:31 -0500

    wrote out the pseudocode and working solution code half from memory and half from suggested solution for breadth first search with path

--- Lambda_CS_TL_algorithms/graphII.py
+++ Lambda_CS_TL_algorithms/graphII.py
@@ -47,10 +47,39 @@ While stack is not empty (length or size of Stack is more than 0):
 '''
 
 '''DFT recursive pseudocode: 
-Will take starting_vertex and visited set to None as the two function parameters
-Import Stack class and instantiate a stack object from Stack class
-Create an empty set called visited
-Push the starting vertex into the back of the stack
+1. Will take starting_vertex and visited set to None as the two function parameters
+2. Import Stack class and instantiate a stack object from Stack class
+3. Create an empty set called visited
+4. Push the starting vertex into the back of the stack
+5. Make a while loop of while size or length of stack is more than 0 
+6. Inside while loop, pop off the last vertex element from the stack and set it to a variable called get_vertex
+7. Make an if statement check if get_vertex (last popped off element from stack) is not in visited then print the vertex and add it to the visited
+set.
+8. Inside the if statment, loop through all the neighbor vertices of the last vertex that was popped off from the stack (get_vertex) with for
+in loop.
+9. Inside for in loop, recursive call the dft_recursive function passing in the neighbor_vertex as the now starting vertex into the function call, 
+and also visited as the second parameter
+'''
+
+''' BFS with path pseudocode:
+1. Will take in starting_vertex and destination_vertex as input parameters
+2. Create an empty set call it visited
+3. Instantiate a new queue called q with the Queue class
+4. Enqueue the starting_vertex into the q as a list (array)
+5. Make a while loop of while q is not empty (q length or size is more than 0)
+6. Remove the first list from the q by dequeueing and set it to a variable called path.
+7. Get the last element from the path list and set it to a variable called get_vertex
+8. Make an if statement, check if the last element from path list, get_vertex is not in visited
+9. Inside previous if statement, make another if statement, check if last element from the path list (get_vertex) is equal to destination_vertex
+then return the path list
+10. Otherwise, (if last element from path list is not the destination_vertex) then add the get_vertex to the visited set
+11. Then (again if last path element is not destination_vertex) loop through the neighbor vertices of get_vertex (last vertex element from path) 
+with a for in loop, name the neighbor vertices neighbor_vertex
+12. Inside the for in loop, make a copy of the path, call it path_copy and append the neighbor_vertex into the copied path, path_copy
+11. Add the copied path, path_copy into the back of the q by enqueing. While loop will continue till all last elements are in visited or a 
+destination_vertex equals the last vertex from path.
+12. Otherwise, outside while loop, return None
+
 '''
 
 class Graph:
@@ -102,6 +131,7 @@ class Graph:
 
                 for neighbor_vertex in self.vertices[get_vertex]:
                     stack.push(neighbor_vertex)
+
     def dft_recursive(self, starting_vertex, visited=None):
         if visited is None:
             visited = set()
@@ -111,12 +141,34 @@ class Graph:
         while stack.size() > 0:
             get_vertex = stack.pop()
 
+            # base case
             if get_vertex not in visited:
                 print(get_vertex)
                 visited.add(get_vertex)
 
                 for neighbor_vertex in self.vertices[get_vertex]:
                     self.dft_recursive(neighbor_vertex, visited)
+
+    def bfs(self, starting_vertex, destination_vertex):
+        visited = set()
+        q = Queue()
+        q.enqueue([starting_vertex])
+
+        while q.size() > 0:
+            path = q.dequeue()
+            get_vertex = path[-1]
+
+            if get_vertex not in visited:
+                if get_vertex == destination_vertex:
+                    return path
+                visited.add(get_vertex)
+
+                for neighbor_vertex in self.vertices[get_vertex]:
+                    path_copy = path.copy()
+                    path_copy.append(neighbor_vertex)
+                    q.enqueue(path_copy)
+
+        return None
 
 
 if __name__ == '__main__':
@@ -179,13 +231,13 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
-    graph.dft_recursive(1)
+    # graph.dft_recursive(1)
     
     '''
     Valid BFS path:
         [1, 2, 4, 6]
     '''
-    # print(graph.bfs(1, 6))
+    print(graph.bfs(1, 6))
     
     '''
     Valid DFS paths:
