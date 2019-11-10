be975d93e48aca70ce29c2a407832ab70a689210
Sem8 <mssemmi8@gmail.com>
2019-11-10 03:16:36 -0500

    wrote out pseudocode and actual solution code for recursive method for checking if a value is in a binary search tree, recursive method for getting the max value in a binary search tree and iterative method for getting the max value in a binary search tree

--- Lambda_CS_TL_algorithms/binary_search_tree.py
+++ Lambda_CS_TL_algorithms/binary_search_tree.py
@@ -20,6 +20,43 @@ the right side of the tree till it hits the base case of if there are no more no
 new node using the BinarySearchTree class with the value passed in and be set as the new right node after the last right node.   
 '''
 
+''' contains method pseudocode: recursive approach
+1. Takes in target value as a parameter to see if binary search tree contains that input target value
+2. Make if statement check if current node value is equal to input target_value then return True.
+3. Otherwise, if input target_value is less than current node value of self.value then make another inner if statement, check if self.left
+does not exist from the current node then return False. 
+4. Else (if a left node from current node does exist) then recursively call the contains method on the next left node passing in the input
+target value as the parameter into the recursive function. This will go to the next left node and if input target_value is equal to this 
+next left node value it'll hit base case and return otherwise if we hit the last left node and still don't find the input target value then
+it'll return False.
+5. Make an elif statement outside of if statement from step 2 to check if input target value is greater than current node at self.value then
+make another inner if statement, check if next node at the right (self.right) does not exist then return False.
+6. Else, for inner else statment (if node right of current node does exist) then recursively call the contains method on the right node of
+tree passing in input target value into the recursive function. This will check the next node on right side of current node to see if that
+node's value matches input target value. If last right side node is reached and still doesn't match input target value and there are no more
+nodes on right side then it'll return False.  
+'''
+
+''' get_max_recursive method: 
+1. Doesn't take in any parameters. Get maximum value in binary search tree by recursive approach 
+2. Make a base case of if binary search tree itself as self does not exist then just return
+3. Otherwise if self (binary search tree) does exist, make another if statement which'll be another base case of if there are no more nodes 
+on the right after the current node then return the value of the current node with self.value
+4. Else (if there are nodes on the right from the current node, so if self.right exists) then recursively call the get_max_recursive 
+function on the right side of binary search tree (self.right)
+'''
+
+''' get_max_iterative method:
+1. Initialize a variable called max_value and set it equal to current node's value of self.value
+2. Set the current node to a variable called current
+3. Make a while loop of while current (node of binary tree) exists.
+4. Inside while loop, make an if statement, check if current node's value is more than the current max_value then re-set the current 
+max_value to current.value
+5. Outside if statement, go to next highest node by setting current to current.right. The if statement will run again and again for the 
+right side nodes until there are no more nodes on right side.
+6. Outside while loop, return the latest re-set max_value
+'''
+
 class BinarySearchTree:
     def __init__(self, value):
         self.value = value
@@ -39,6 +76,43 @@ class BinarySearchTree:
                 self.right = BinarySearchTree(value)
             else:
                 self.right.insert(value)
+
+    def contains(self, target_value):
+        if not self:
+            return
+        if self.value == target_value:
+            return True
+        if target_value < self.left:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target_value)
+        elif target_value > target_value:
+            if not self.right:
+                return False
+            else:
+                self.right.contains(target_value)
+
+    def get_max_recursive(self):
+        if not self:
+            return
+
+        if not self.right:
+            return self.value
+        else:
+            return self.right.get_max_recursive()
+
+    def get_max_iterative(self):
+        max_value = self.value
+        current = self
+
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.right
+
+        return max_value
+
                 
 bst = BinarySearchTree(1)
 
