e318bd0b5e3bc33d53956e3a8354cd56f098b660
Sem8 <mssemmi8@gmail.com>
2019-12-30 20:52:22 -0500

    wrote out the pseudocode and suggested solution code but with some of my own code logic modifications to pass the edge case tests for recursive solution for decoding number of ways a number can be decoded to alphabets

--- Leetcode_Problems/decodeWays.js
+++ Leetcode_Problems/decodeWays.js
@@ -17,6 +17,28 @@ Input: "226"
 Output: 3
 Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6). */
 
+// const numDecodings = s => {
+//     let msgLen = s.length;
+//     if (parseInt(s) === 0) {
+//         return 0;
+//     };
+//     if (msgLen === 0 || msgLen === 1) {
+//         return 1;
+//     };
+
+//     let count = 0;
+
+//     if (parseInt(s[msgLen - 1]) > 0) {
+//         count = numDecodings(s.substring(0, msgLen-1));
+//     };
+
+//     if (parseInt(s.substring(msgLen-2)) < 27 && parseInt(s.substring(msgLen-2)) >= 10) {
+//         count += numDecodings(s.substring(0, msgLen-2));
+//     };
+
+//     return count;
+// };
+
 // My solution: Doesn't work for input numbers of more than 3 numbers
 // Make an object with numbers as keys and letters as values
 // Declare an empty object, call it result, return length of it at end of function with Object.keys(result).length
@@ -28,37 +50,10 @@ Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6). 
 // Then split that s number string, get the letter of each number from the array of split up numbers with for of loop and push those
 // letters into the result array
 // Otherwise, if length of s is more than 2, put the original s number
-/*Pseudocode:
- */
-
-const letterMapper = {
-  1: "A",
-  2: "B",
-  3: "C",
-  4: "D",
-  5: "E",
-  6: "F",
-  7: "G",
-  8: "H",
-  9: "I",
-  10: "J",
-  11: "K",
-  12: "L",
-  13: "M",
-  14: "N",
-  15: "O",
-  16: "P",
-  17: "Q",
-  18: "R",
-  19: "S",
-  20: "T",
-  21: "U",
-  22: "V",
-  23: "W",
-  24: "X",
-  25: "Y",
-  26: "Z"
-};
+// const letterMapper = {
+//   1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H", 9: "I", 10: "J", 11: "K", 12: "L", 13: "M", 14: "N", 15: "O", 16: "P", 
+//   17: "Q", 18: "R", 19: "S", 20: "T", 21: "U", 22: "V", 23: "W", 24: "X", 25: "Y", 26: "Z"
+// };
 // console.log(letterMapper);
 
 // const numDecodings = s => {
@@ -110,7 +105,7 @@ const letterMapper = {
 //   recurse(numStr, (prev += numStr.substr(index, 2)), index + 1, result, letterMapper);
 // };
 
-// Suggested Solution 1:
+// Suggested Solution 1 (iterative):
 /* Pseudocode:
 1. Make an if statement of if input s length is 0 (input string number is an empty string) then just return 0
 2. Get the length of the input s number string and set it to a variable called len
@@ -130,30 +125,69 @@ value of dp array at current index i plus equal to value of dp array from index 
 or equal to 26 then set the value of dp array at current index plus equals value from dp array at two indices before current index (i - 2)
 10. Outside for loop, return the value from dp array at the index of length of input s
  */
+// const numDecodings = s => {
+//     if (s.length === 0) {
+//         return 0;
+//     };
+
+//     let len = s.length;
+//     let dp = new Array(len + 1).fill(0);
+    
+//     dp[0] = 1;
+//     dp[1] = s[0] != '0' ? 1 : 0;
+
+//     for (let i = 2; i <= len; i++) {
+//         let first = parseInt(s[i-1]);
+//         let second = parseInt(s.substring(i-2, i));
+
+//         if (first > 0 && first < 10) {
+//             dp[i] += dp[i-1];
+//         };
+//         if (second >=10 && second <= 26) {
+//             dp[i] += dp[i-2];
+//         }
+//     };
+//     return dp[len];
+// };
+
+// Suggested solution 2 (recursive):
+/* Pseudocode:
+1. Make a base case of if integer version of input number string s (using parseInt method) is 0 then just return 0 (there are no or 0 ways 
+    to decode the number 0).
+2. Get the length of the input number string and set it to a variable called msgLen
+3. Make another base case of if input number string length, msgLen is 0 or 1 (1 number long or empty string) return 1
+4. outside previous if statement, make a variable called count and initialize it to 0
+5. Make an if statement of if the integer version of the last number from input string s (using parseInt at index of msgLen) is more than 0
+then set count equal to recursive call of numDecodings function, passing in as parameter the the number string but without the last character
+using substring method (characters 0 index to the 2nd to last character)
+6. Outside previous if statement, make another if statement of if the last 2 characters of the input number string s (using substring method)
+is less than 27 or more than or equal to 10 then set count to plus equal recursive call of numDecodings function, passing in as parameter,
+the input number string without the last 2 numbers of that number string (using substring method to exclude the last 2 number characters)
+7. Outside the if statement return the count variable
+ */
+
 const numDecodings = s => {
-    if (s.length === 0) {
+    if (s === '0') {
         return 0;
     };
 
-    let len = s.length;
-    let dp = new Array(len + 1).fill(0);
-    
-    dp[0] = 1;
-    dp[1] = s[0] != '0' ? 1 : 0;
+    let msgLen = s.length;
+    let count = 0;
 
-    for (let i = 2; i <= len; i++) {
-        let first = parseInt(s[i-1]);
-        let second = parseInt(s.substring(i-2, i));
-
-        if (first > 0 && first < 10) {
-            dp[i] += dp[i-1];
-        };
-        if (second >=10 && second <= 26) {
-            dp[i] += dp[i-2];
-        }
+    if (msgLen === 0 || msgLen === 1) {
+        return 1;
     };
-    return dp[len];
-};
+    
+    if(parseInt(s[msgLen-1]) > 0) {
+        count = numDecodings(s.substring(0, msgLen-1));
+    };
+
+    if (parseInt(s.substring(msgLen-2)) < 27 && parseInt(s.substring(msgLen-2)) >= 10) {
+        count += numDecodings(s.substring(0, msgLen-2));
+    };
+
+    return count;
+}
 
 
 console.log(numDecodings("12"));     // 2
@@ -162,3 +196,7 @@ console.log(numDecodings("27"));     // 1
 console.log(numDecodings("226"));    // 3
 console.log(numDecodings("227"));    // 2
 console.log(numDecodings("2266"));   // 3
+console.log(numDecodings('01'));    // 0
+console.log(numDecodings('10'));    // 1
+
+// console.log(parseInt('01'));
