8d234fbf6dd4fda107661f1f6a0ca12a62f51410
Sem8 <mssemmi8@gmail.com>
2020-02-16 19:31:15 -0500

    wrote my working code and pseudocode for the depth first search with path algorithm for graphs

--- Lambda_CS_TL_algorithms/graph_dfs_bfs.js
+++ Lambda_CS_TL_algorithms/graph_dfs_bfs.js
@@ -13,6 +13,23 @@ add node element to the visited set
 vertex of get_vertex
 11. Add the new path array(s) to the back of the queue
  */
+
+/* DFS Path pseudocode:
+1. Declare a Stack class instantiatin, call it stack
+2. Declare an empty set, call it visited
+3. Push the starting_vertex as an array into the back of stack
+4. While stack is not empty (stack size is more than 0)
+5. Inside while loop, pop the last array from back of stack, name it path & get the last value from path, name it get_vertex
+6. Still inside while loop, check if get_vertex is not in visited set then put it in visited set in the if statement.
+7. Still inside the previous if statement of if get_vertex is not in visited then check if get_vertex is equal to target_value then 
+return path. Otherwise...
+8. Still inside the previous if statement of if get_vertex is not in visited, get the neighbors set of get_vertex vertex from the 
+self.vertices dictionary and call it neighbor_vertices
+9. Make a helper function called pathAdder where it takes in value as an argument, and inside function, we make a new copy of the path
+with path.slice() method, call it new_path, push the input values into the back of the new_path then push the new_path to the back of
+the stack
+10. Call the pathAdder function on each vertex of the neighbor_vertices set using the forEach sets method.
+ */
 class Queue {
   constructor() {
     this.queue = [];
@@ -65,7 +82,7 @@ class Graph {
   add_edge(v1, v2) {
     if (v1 in this.vertices && v2 in this.vertices) {
       this.vertices[v1].add(v2);
-      this.vertices[v2].add(v1);
+      // this.vertices[v2].add(v1);
     } else {
       console.log("The vertex does not exist!");
     }
@@ -84,29 +101,57 @@ class Graph {
 
     while (queue.size() > 0) {
       let path = queue.dequeue();
-    //   console.log("path: ", path);
+      //   console.log("path: ", path);
       let get_vertex = path[path.length - 1];
-    //   console.log("get_vertex: ", get_vertex);
-
-      //   let visited_values = visited.values();
+      //   console.log("get_vertex: ", get_vertex);
 
       if (visited.has(get_vertex) == false) {
         visited.add(get_vertex);
         // console.log("visited: ", visited);
 
         if (get_vertex == target_value) {
-        //   console.log(path);
+          //   console.log(path);
           return path;
         }
         // console.log('self.vertices[get_vertex]: ', this.vertices[get_vertex]);
         let neighbor_vertices = this.vertices[get_vertex];
-        let pathAdder = (values) => {
-            let new_path = path.slice();
-            new_path.push(values);
-            queue.enqueue(new_path);
-        }
+        let pathAdder = values => {
+          let new_path = path.slice();
+          new_path.push(values);
+          queue.enqueue(new_path);
+        };
         neighbor_vertices.forEach(pathAdder);
+      }
+    }
 
+    return null;
+  }
+
+  dfs_path(starting_vertex_id, target_value) {
+    let stack = new Stack();
+    let visited = new Set();
+    stack.push([starting_vertex_id]);
+
+    while (stack.size() > 0) {
+      let path = stack.pop();
+      let get_vertex = path[path.length - 1];
+
+      if (visited.has(get_vertex) == false) {
+        visited.add(get_vertex);
+
+        if (get_vertex == target_value) {
+          return path;
+        }
+
+        let neighbor_vertices = this.vertices[get_vertex];
+
+        let pathAdder = values => {
+          let new_path = path.slice();
+          new_path.push(values);
+          stack.push(new_path);
+        };
+
+        neighbor_vertices.forEach(pathAdder);
       }
     }
 
@@ -133,10 +178,18 @@ graph.add_edge(3, 5);
 graph.add_edge(2, 3);
 graph.add_edge(4, 6);
 
-// console.log("self.vertices: ", graph.vertices);
+// console.log("graph.vertices: ", graph.vertices);
 
 /*
 Valid BFS path:
     [1, 2, 4, 6]
 */
-console.log(graph.bfs_path(1, 6));
+// console.log(graph.bfs_path(1, 6));
+
+/*
+Valid DFS paths:
+    [1, 2, 4, 6]
+    [1, 2, 4, 7, 6]
+*/
+
+console.log(graph.dfs_path(1, 6));
