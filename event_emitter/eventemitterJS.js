0959970eb19a62ddeca79e792683e886d01153e2
Sem8 <mssemmi8@gmail.com>
2020-02-25 03:35:32 -0500

    wrote out pseudocode for event emitter class and methods in vanilla javascript

---
+++ event_emitter/eventemitterJS.js
@@ -0,0 +1,28 @@
+/* Event emitters take in an event name as an argument for an event and a specific callback function to execute for that event name 
+as the second argument. This is the on method which in addition to defining an eventName will also define how the callback is executed for that
+specific event name.
+
+Event emmitters also have a trigger method so that when you call that method, the method also takes in the event name for the 1st argument,
+then calls all the callback functions for that specific event name
+*/
+
+/* Pseudocode for creating event emitter:
+
+EventEmitter class: 
+1. Create an EventEmitter class which has just one attribute which is a this.events object. 
+
+EventEmitter 'on' method:
+1. Create a method called 'on' in the EventEmitter class which takes in 2 arguments, eventName and callback
+2. Make an if statement, check if eventName already exists in the events object as a key (with values of array of callbacks) and if it does then 
+just push the new input callback argument into the array of callbacks as values to the eventName key into the events object.
+3. If eventName doesn't already exist in the events object as a key then just set the eventName as a key to the events object with a value of 
+the input callback as an array
+
+EventEmitter 'trigger' method: 
+1. Create a method called 'trigger' in the EventEmitter class which takes in 2 arguments, eventName and rest parameter with spread operator for 
+arguments
+2. In the trigger method, take the array value (array of callbacks) of the eventName key from events object and run a forEach method on that array
+of callback functions. In the forEach method, name each array element of callbacks, cb and use the apply method to each cb callback function to 
+pass in the arguments of rest 
+
+*/
