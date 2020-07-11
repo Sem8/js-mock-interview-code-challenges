7cc5296ebb428848ef37eb17e7bca2f7532a1df8
Sem8 <mssemmi8@gmail.com>
2020-07-11 00:51:55 -0400

    wrote pseudocode and solution code for missing number code challenge from a solution from leetcode with 85% time complexity beat with O(n) solution

--- ideaEvolverPractice/missingNumber.js
+++ ideaEvolverPractice/missingNumber.js
@@ -18,20 +18,40 @@ Your algorithm should run in linear runtime complexity. Could you implement it u
  plus 1
   */
 
+// var missingNumber = function (nums) {
+//   nums.sort((a, b) => {
+//     return a - b;
+//   });
+//     if (nums[0] > 0) {
+//       return 0;
+//     }
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i + 1] > nums[i] + 1) {
+//       return nums[i] + 1;
+//     } 
+//   }
+//   return nums[nums.length - 1] + 1;
+// };
+
+// Solution 2 from leetcode - 86.5% runtime beat
+/* Pseudocode:
+- Have two variables called sum and total and initialize both to 0. Sum will be the sum of all the array elements 
+from input nums array and total will be the sum of the index plus 1 -- So total will always be 1 number ahead of 
+the array element for each element. 
+ex. sorted array = [0, 1, 2] -- sum = [0, 1, 3] -- total = [1, 3, 6]
+- Loop through the array with for loop and let current sum plus equal current array element and let current total plus equal
+current index + 1
+- Outside for loop, return total - sum where total will always be 1 above sum 
+ */
 var missingNumber = function (nums) {
-  nums.sort((a, b) => {
-    return a - b;
-  });
-    if (nums[0] > 0) {
-      return 0;
+    let sum = 0;
+    let total = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        total += i + 1;
     }
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i + 1] > nums[i] + 1) {
-      return nums[i] + 1;
-    } 
-  }
-  return nums[nums.length - 1] + 1;
-};
+    return total - sum;
+  };
 
 console.log(missingNumber([3, 0, 1])); // 2
 console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])); // 8
